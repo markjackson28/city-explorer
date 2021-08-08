d19f84c47dacc2448773423805104f9b4a83d0b2
Mark Thanadabouth <mark28ten@gmail.com>
2021-08-07 17:37:26 -0700

    added poster

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LAB - 06
+# LAB - 09
 
 ## City Explorer
 
@@ -64,3 +64,15 @@ Start time: 2:50pm (08/03)
 Finish time: 4:02pm (08/06)
 
 Actual time needed to complete: 8 Hours
+
+### LAB - 09
+
+Name of feature: Add live weather/server
+
+Estimate of time needed to complete: 7 Hours
+
+Start time: 1:00pm
+
+Finish time: 
+
+Actual time needed to complete: 
--- src/Movies.js
+++ src/Movies.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card'
-import CardColumns from 'react-bootstrap/CardColumns';
-import Container from 'react-bootstrap/Container'
+import Movie from './Movie'
+
 
 class Movies extends React.Component {
   render() {
-    let cardItem = this.props.movieData.map((movie, index) => (
-      <Card>
-        {/* <Card.Img variant="top" src={movie.img_url} /> */}
-        <Card.Header><h4 key={index}>{movie.title}</h4></Card.Header>
-        <Card.Body>
-          <Card.Title>Movie Description</Card.Title>
-          {/* <Card.Title><h4 key={index}>{movie.title}</h4></Card.Title> */}
-          <Card.Text>{movie.overview}</Card.Text>
-        </Card.Body>
-      </Card>
-    ));
-
     return (
       <>
-        {this.props.renderMovieData ?
-          <Container>
-            <CardColumns>
-              {cardItem}
-            </CardColumns>
-          </Container>
-          : ''}
+        {this.props.renderMovieData ? <Movie movieData={this.props.movieData}/> : ''}
       </>
     )
   }
